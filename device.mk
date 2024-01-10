5f1f6e90f2b48169fb65384f3cf6b099aa44ac75
Ij Israfil <israfilkhan494@gmail.com>
2024-01-10 16:12:15 +0600

    RMX1901: Remove QuickAccessWallet
    Signed-off-by: ij-israfil <israfilkhan494gmail.com>

--- device.mk
+++ device.mk
@@ -496,10 +496,6 @@ PRODUCT_PACKAGES += \
     vndk_package \
     com.android.vndk.current.on_vendor
 
-# Wallet
-PRODUCT_PACKAGES += \
-    QuickAccessWallet
-
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     libnl \
