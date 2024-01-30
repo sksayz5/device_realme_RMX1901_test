075e52edf9879a5213ca7a524aff1992b9352a82
Georg Veichtlbauer <georg@vware.at>
2024-01-30 09:29:36 +0100

    RMX1901: powerhint: Add DT2W
    Change-Id: I7d0dfe249798864a39afd4f339b692584faaf3ec
    Signed-off-by: Shravan Sayz <shravankumarsayz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,8 +130,6 @@ BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_ODM := odm
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
