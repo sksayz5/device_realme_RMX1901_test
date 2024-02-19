cdfc72bef8f31866a2df26e24937c6463bc23e92
ij-israfil <israfilkhan494@gmail.com>
2024-02-19 08:58:28 +0600

    Revert "RMX1901: Nuke offendering entries"
    This reverts commit 88c50c63b6b5e41973226d9e1a4801dab3cf3314.

--- device.mk
+++ device.mk
@@ -56,6 +56,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libvolumelistener \
+    libaudioparsers \
     libcirrusspkrprot \
     libspkrprot \
     libhdmiedid \
@@ -64,6 +65,7 @@ PRODUCT_PACKAGES += \
     libhfp \
     libbatterylistener \
     libsndmonitor \
+    libhdmipassthru \
     tinymix
 
 PRODUCT_COPY_FILES += \
