0971a5c58c1529c48a6cd2e5eb4f1d5c953d0582
shravansayz <shravankumarsayz@gmail.com>
2024-08-14 09:48:23 +0530

    RMX1901: Adapt to PixelOS
    Signed-off-by: shravansayz <shravankumarsayz@gmail.com>

--- aosp_RMX1901.mk
+++ aosp_RMX1901.mk
@@ -4,15 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff
+# Inherit some common PixelOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_MINI_GAPPS:= true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+WITH_GAPPS := true
 TARGET_DISABLE_EPPE := true
+TARGET_GAPPS_ARCH := arm64
 
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 
 # Inherit from RMX1901 device
@@ -25,7 +27,7 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := aosp_RMX1901
 PRODUCT_MODEL := RMX1901
 
 PRODUCT_SYSTEM_NAME := RMX1901
