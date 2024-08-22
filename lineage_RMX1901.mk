9d447a4db1eec14b9b5880f2d3c2e553fbc4f29a
shravansayz <shravankumarsayz@gmail.com>
2024-08-22 18:56:22 +0530

    RMX1901: Los-ext
    Signed-off-by: shravansayz <shravankumarsayz@gmail.com>

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -9,7 +9,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_MINI_GAPPS:= true
+WITH_GMS := true
 TARGET_DISABLE_EPPE := true
 
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
