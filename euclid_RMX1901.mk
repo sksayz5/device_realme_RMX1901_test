5c9e8e0ab0a0f0100fa4220588bb173af130dcc6
shravansayz <shravankumarsayz@gmail.com>
2024-09-10 20:15:58 +0530

    RMX1901: EuclidBringup
    Signed-off-by: shravansayz <shravankumarsayz@gmail.com>

--- euclid_RMX1901.mk
+++ euclid_RMX1901.mk
@@ -7,13 +7,14 @@
 # Inherit some common Evolution X stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
-TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_MINI_GAPPS:= true
-BUILD_SEEDVAULT := true
-TARGET_DISABLE_EPPE := true
+EUCLID_MAINTAINER := Shravan
+EUCLID_GAPPS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
 
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
 
 
 # Inherit from RMX1901 device
@@ -26,7 +27,7 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := euclid_RMX1901
 PRODUCT_MODEL := RMX1901
 
 PRODUCT_SYSTEM_NAME := RMX1901
